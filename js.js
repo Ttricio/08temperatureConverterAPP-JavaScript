fdf9c68adc803c919daa40c8002b96a918aa8ca2
Patryk Kaczmarek <patrykmkaczmarek@gmail.com>
2022-10-12 12:01:58 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,54 @@
+const converter = document.querySelector('#converter')
+const result = document.querySelector('.result')
+const convBtn = document.querySelector('.conv')
+const resetBtn = document.querySelector('.reset')
+const changeBtn = document.querySelector('.change')
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+
+let farenheit
+let celsius
+
+const swap = () => {
+    if(one.textContent === '°C') {
+        one.textContent =  '°F'
+        two.textContent =  '°C'
+        result.textContent = " "
+    }else {one.textContent =  '°C'
+    two.textContent =  '°F'
+.tofixed(1)
+    }
+}
+
+
+
+const fahrToCel = () => {
+farenheit = converter.value * 1.8 + 32
+result.textContent = `${converter.value}°C to ${farenheit.toFixed(1)}°F `
+    converter.value = ''
+}
+const celToFahr = () => {
+    celsius = (converter.value - 32)/ 1.8 
+    result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C `
+    
+}
+const conversion = () => {
+    if(converter.value !== ''){
+        if(one.textContent === '°C'){
+            fahrToCel()
+        }else{
+            celToFahr()
+        }
+    }else{
+        result.textContent = 'Musisz podać wartość'
+    }
+}
+
+const reset = () => {
+    converter.value = ''
+    result.textContent = ''
+}
+
+changeBtn.addEventListener('click', swap)
+convBtn.addEventListener('click', conversion)
+resetBtn.addEventListener('click', reset)
